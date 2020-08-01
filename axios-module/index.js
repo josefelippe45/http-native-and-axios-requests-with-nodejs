1b6812500b0f1ae83eef769d18fb9b1ee2323a58
José Felippe Gonçalves Oliveira <jose.oliveira370@etec.sp.gov.br>
2020-08-01 16:31:11 -0300

    axios request

--- axios-module/index.js
+++ axios-module/index.js
@@ -2,6 +2,8 @@ const axios = require('axios');
 let username= 'josefelippe45';
 
 //then is a promise like an information that we are promised to get back
+//here we get the information of the user
 axios.get('https://api.github.com/users/' + username).then((res)=>{
+    //we can access the properties of this object like res.data.followers
     console.log(res.data);
 });
